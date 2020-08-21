eb053d89ea487bfc500ea4b7dd866643fb82cbce
Soso Janashvili <sosojanashvili@gmail.com>
2020-08-21 22:20:37 +0400

    Added floyd-warshall algorythm f-s

---
+++ src/floyd_warshall.c
@@ -0,0 +1,38 @@
+//
+// Created by Soso Janashvili on 21.08.2020.
+//
+
+#include "../inc/pathfinder.h"
+
+static void initialize(int *graph, int *dis, int *next, int size) {
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size; j++) {
+            *(dis + (i * size) + j) = *(graph + (i * size) + j);
+        }
+    }
+}
+
+void floyd_warshall(int *graph, int size) {
+
+    int *dis = (int *)malloc(size * size * sizeof(int));
+    int *next = (int *)malloc(size * size * sizeof(int));
+
+    initialize(graph, dis, next, size);
+
+    for (int k = 0; k < size; k++) {
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                if (*(dis + (i * size) + k) == INF ||
+                    *(dis + (k * size) + j) == INF)
+                    continue;
+
+                if (*(dis + (i * size) + j) > *(dis + (i * size) + k) + *(dis + (k * size) + j)) {
+                    *(dis + (i * size) + j) = *(dis + (i * size) + k) + *(dis + (k * size) + j);
+                    *(next + (i * size) + j) = *(next + (i * size) + k);
+                }
+            }
+        }
+    }
+    print_graph(dis, size);
+    //free *next *dis
+}
